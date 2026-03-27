3e5a4307b7b5762b11cfe484897b20c220699037
Rishav Patel <156516288+RishavPatel1@users.noreply.github.com>
2026-03-27 15:39:09 +0530

    reuirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="AI_Medical_Assistant",
+    version="0.1.0",
+    author="Rishav Patel",
+    author_email="krishav406@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
